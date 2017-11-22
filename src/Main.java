bb86eaac4c46630ed0e244fbc069b9b4aef9dd25
Kamilex2303 <kamil.stasiak97@icloud.com>
2017-11-22 15:16:34 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -2,44 +2,46 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 
 import java.sql.*;
 import java.util.Scanner;
-public class Main
-{
-    public static void main( String args[] )
-    {
-        Connection c = null;
-        Statement stmt = null;
-        Scanner read = new Scanner(System.in);
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:main");
-            c.setAutoCommit(true);
-            System.out.println("Opened database successfully");
-            stmt = c.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM osoba;" );
-            System.out.print("Podaj minimalny rocznik : ");
-            int rok = read.nextInt();
-            while ( rs.next() ) {
-                String  imie = rs.getString("imie");
-                int rocznik = rs.getInt("rocznik");
-                int pensja  = rs.getInt("pensja");
-                String upPensja = "UPDATE osoba set pensja = pensja*1.1 where rocznik >= "+rok+";";
-                stmt.executeUpdate(upPensja);
-            }
 
-            System.out.println("\n");
+public class Main {
 
-            rs = stmt.executeQuery( "SELECT * FROM osoba;" );
-            while ( rs.next() ) {
-                String imie = rs.getString("imie");
-                int rocznik = rs.getInt("rocznik");
-                int pensja = rs.getInt("pensja");
-                if(rocznik>=rok)
-                System.out.println("Imie : " + imie + " , rocznik : " + rocznik + " , pensja : " + pensja);
-            }
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
+
+    static void print(int rok) throws SQLException {
+        Connection c = DriverManager.getConnection("jdbc:sqlite:main");
+        Statement stmt;
+        stmt = c.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM osoba;");
+        while (rs.next()) {
+            String imie = rs.getString("imie");
+            int rocznik = rs.getInt("rocznik");
+            int pensja = rs.getInt("pensja");
+            if(rocznik>= rok)
+            System.out.println(imie + " , " + rocznik + " , " + pensja);
         }
-        System.out.println("Operation done successfully");
+
     }
+
+    static void upSalary(int rok) throws SQLException {
+        Connection c = DriverManager.getConnection("jdbc:sqlite:main");
+        Statement stmt;
+        stmt = c.createStatement();
+        String upMoney = "UPDATE osoba set pensja = pensja*1.1 where rocznik >= "+rok+";";
+        stmt.executeUpdate(upMoney);
+
+    }
+
+
+
+    public static void main(String[] args) throws SQLException {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Podaj rok od ktorego sprawdzac ( w gore ) : ");
+        int rok = read.nextInt();
+        print(rok);
+        upSalary(rok);
+        System.out.printf("\n");
+        print(rok);
+
+    }
+
+
 }
